173458c20abd4395da38e6561df447b3d3b92c7e
kuhaku001 <jorge.fj.carcamo@gmail.com>
2023-11-17 10:14:45 -0300

    etiquetas MVC

--- src/controller/etiquetasController.js
+++ src/controller/etiquetasController.js
@@ -1,105 +1,58 @@
-const etiquetaModels = require("../database/etiquetaModel");
-const Token = require('../models/token')
+const Etiquetas = require('../models/etiquetas')
 
 exports.crearEtiqueta = async (req, res) => { 
-    if(await Token(req, "administrador")){
-        try {
+    try {   
+        const etiqueta = Etiquetas.crearEtiqueta(req, req.body);
+        res.send(etiqueta);
             
-            const etiqueta = etiquetaModels(req.body);
-
-            await etiqueta.save();
-            res.send(etiqueta);
-            
-        } catch (error) {
-            console.log(error);
-            res.status(500).send('Hubo un error');
-        }
-    } else {
-        res.status(400).send('Acceso denegado');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
     }
 }
 
 exports.obtenerEtiquetas = async (req, res) => {
-    if(await Token(req, "administrador")){
-        try {
-            const etiqueta = await etiquetaModels.find({tipo: ""})
-
-            res.json(etiqueta)
+    try {
+        const etiqueta = await Etiquetas.obtenerEtiquetas(req)
+        res.json(etiqueta)
             
-        } catch (error) {
-            console.log(error);
-            res.status(500).send('Hubo un error');
-            
-        } 
-    } else {
-        res.status(400).send('Acceso denegado');
-    }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    } 
 }
 
 exports.actualizarEtiqueta = async (req, res) => {
-    if(await Token(req, "administrador")){
-        try {
-            const { name,color_etiqueta } = req.body;
-            let etiqueta = await etiquetaModels.findById(req.params.id);
-
-            if(!etiqueta){
-                res.status(404).json({msg:'no existe la etiqueta'})
-            }
-            etiqueta.name = name
-            etiqueta.color_etiqueta = color_etiqueta
-        
-
-            etiqueta = await etiquetaModels.findOneAndUpdate({_id:req.params.id},etiqueta,{new:true})
-            res.json(etiqueta);
+    try {
+        const etiqueta = await Etiquetas.actualizarEtiqueta(req, req.params.id, req.body);
+        res.json(etiqueta)
             
-            
-        } catch (error) {
-            console.log(error);
-            res.status(500).send('Hubo un error');
-        }
-    } else {
-        res.status(400).send('Acceso denegado');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
     }
 }
 
-
 exports.eliminarEtiqueta = async (req, res) => {
-    if(await Token(req, "administrador")){
-        try {
-            let etiqueta = await etiquetaModels.findById(req.params.id);
-
-            if(!etiqueta) {
-                res.status(404).json({ msg: 'No existe la etiqueta' })
-            }
-
-            await etiquetaModels.findOneAndRemove({_id:req.params.id})
-            res.json(' etiqueta eliminada con  exito');
-            
-        } catch (error) {
-            console.log(error);
-            res.status(500).send('Hubo un error');
-        }
-    } else {
-        res.status(400).send('Acceso denegado');
+    try {
+        const etiqueta = await Etiquetas.eliminarEtiqueta(req, req.params.id);
+        res.json(etiqueta);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
     }
 }
 
 exports.obtenerEtiqueta = async (req, res) => {
-    if(await Token(req, "administrador")){
-        try {
-            let etiqueta = await etiquetaModels.findById(req.params.id);
-
-            if(!etiqueta) {
-                res.status(404).json({ msg: 'No existe la etiqueta' })
-            }
+    try {
+        const etiqueta = await Etiquetas.obtenerEtiqueta(req, req.params.id)
         
-            res.json(etiqueta);
+        res.json(etiqueta);
             
-        } catch (error) {
-            console.log(error);
-            res.status(500).send('Hubo un error');
-        }
-    } else {
-        res.status(400).send('Acceso denegado');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
     }
+
 } 
--- src/models/etiquetas.js
+++ src/models/etiquetas.js
@@ -1,1 +1,80 @@
 const Token = require('./token')
+const Etiquetas = require("../database/etiquetaModel");
+
+exports.crearEtiqueta = async (autorizacion, etiquetaData) => { 
+    if(await Token(autorizacion, "administrador")){
+            
+        const etiqueta = await Etiquetas(etiquetaData);
+
+        await etiqueta.save();
+        return etiqueta
+            
+    } else {
+        return 'Acceso denegado'
+    }
+}
+
+exports.obtenerEtiquetas = async (autorizacion) => {
+    if(await Token(autorizacion, "administrador")){
+
+        const etiqueta = await Etiquetas.find()
+        return etiqueta
+
+    } else {
+        return 'Acceso denegado'
+    }
+}
+
+exports.actualizarEtiqueta = async (autorizacion, id, etiquetaData) => {
+    if(await Token(autorizacion, "administrador")){
+        try {
+            const { name,color_etiqueta } = etiquetaData;
+            let etiqueta = await Etiquetas.findById(id);
+
+            if(!etiqueta){
+                return {msg:'no existe la etiqueta'}
+            }
+            etiqueta.name = name
+            etiqueta.color_etiqueta = color_etiqueta
+            etiqueta = await Etiquetas.findOneAndUpdate({_id : id},etiqueta,{new:true})
+            
+            return etiqueta;
+            
+        } catch (error) {
+            console.log(error);
+            return 'Hubo un error'
+        }
+    } else {
+        return 'Acceso denegado'
+    }
+}
+
+exports.eliminarEtiqueta = async (autorizacion, id) => {
+    if(await Token(autorizacion, "administrador")){
+        let etiqueta = await Etiquetas.findById(id);
+
+        if(!etiqueta) {
+            return { msg: 'No existe la etiqueta' }
+        }
+        await Etiquetas.findOneAndRemove({_id:id})
+
+        return ' etiqueta eliminada con  exito'
+            
+    } else {
+        return 'Acceso denegado'
+    }
+}
+
+exports.obtenerEtiqueta = async (autorizacion, id) => {
+    if(await Token(autorizacion, "administrador")){
+        let etiqueta = await Etiquetas.findById(id);
+
+        if(!etiqueta) {
+            return { msg: 'No existe la etiqueta' }
+        }
+        
+        return etiqueta
+    } else {
+        return 'Acceso denegado'
+    }
+} 
