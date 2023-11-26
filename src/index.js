a15e953782df82907e51102cd5a655e02affb770
kuhaku001 <jorge.fj.carcamo@gmail.com>
2023-11-25 23:02:32 -0300

    test 2

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // esta carpeta sera de uso publico (imagenes de los productos)
-app.use('../uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('../uploads', express.static(path.join(__dirname, 'sistema-inventario-server/uploads')));
 
 app.use('/api/cliente', require('./routes/cliente'));
 app.use('/api/materiales', require('./routes/materiales'));
